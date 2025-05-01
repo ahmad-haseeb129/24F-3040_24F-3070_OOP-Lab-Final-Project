63f6772df5fa418c37255b13a90954cfb8268b7d
ahmad-haseeb129 <ahaseebhaseeb123@gmail.com>
2025-05-02 03:33:52 +0500

    Zip files uploaded
    Ok so I have replaced the folders with zip files.

--- files.cpp/Population.cpp
+++
@@ -1,9 +0,0 @@
-#include "Stronghold.h"
-
-Population::Population() {
-    totalPopulation = 0;  // Will be set by updateTotalPopulation
-}
-
-Population::~Population() {
-    // Nothing to clean up
-}
